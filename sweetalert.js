53986d2103905cf4445420c076d3d2328afaa2f6
AriRubDam <arirub08@gmail.com>
2022-06-18 22:17:47 -0500

    start

---
+++ sweetalert.js
@@ -0,0 +1,12 @@
+Swal.fire({
+    icon: 'info',
+    title: 'Recomendacion',
+    text: 'Usar desde una Computadora',
+    footer: '&copy; Arian - Junio-2022',
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    allowEnterKey: false,
+    showConfirmButton: false,
+    timer: 4000,
+    timerProgressBar: true,
+  })
